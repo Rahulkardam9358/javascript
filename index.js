112fce80532df391316a004ad23b804b6f434f81
Rahul Kardam <rahul.kardam.16@mountblue.tech>
2021-05-24 11:20:13 +0530

    final commit

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-fetch(window.location.href+"/data.json", { mode: 'no-cors'})
+function run(){
+    let url = document.getElementById('input').value;
+    fetch(url, { mode: 'no-cors'})
     .then(function(resp){
         return resp.json();
     })
     .then(function(json_data){
         Highcharts.chart('container',json_data);
     });
+};
